d244418c4e710beac8dad88d5853c0b024ff2765
Arseny Tomilov <tomilovsenya@ya.ru>
2022-08-12 12:50:02 +0500

    Lobby menu logic adjustments

--- js/lobby_menu.js
+++ js/lobby_menu.js
@@ -4,6 +4,32 @@ let activeEntryMiddle = null;
 
 $(".menu_category").click(clickCategory);
 
+function triggerCategory(triggeredCategory) {
+  // Return if empty
+  if (triggeredCategory.is($(".menu_entry_empty_double"))) return;
+  if (triggeredCategory.is($(".menu_entry_empty"))) return;
+  if (activeCategory == null) {
+    triggeredCategory.trigger("categoryActive");
+    console.log("Triggered category: " + triggeredCategory.attr("id"));
+  } else if (activeCategory.is(triggeredCategory)) {
+    console.log("Triggered already active category, will only enter entries middle: " + triggeredCategory.attr("id"));
+    // Enter entries middle
+    if (!triggeredCategory.is(".menu_category_enter")) return;
+    if (!isCategorySelected) {
+      console.log("Entering entries middle from triggerCategory");
+      enterMenuEntriesMiddle(activeWindow.cats[triggeredCategory.index()]);
+    } else if (isCategorySelected) {
+      escapeMenuEntriesMiddle();
+    }
+  } else if (activeCategory != triggeredCategory) {
+    escapeMenuEntriesMiddle();
+    disableCategory(activeCategory);
+    activeCategory = triggeredCategory;
+    triggeredCategory.trigger("categoryActive");
+    console.log("Triggered category: " + triggeredCategory.attr("id"));
+  }
+}
+
 function clickCategory() {
   setEntryActive($(this));
   if ($(this).attr("id")) console.log("Clicked: " + $(this).attr("id"));
