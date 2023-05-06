77b93f56c2873e09f8e9cb96554ca62a8eda6bc4
Arseny Tomilov <tomilovsenya@ya.ru>
2023-05-06 16:11:26 +0500

    Implement scripting

--- js/lobby_menu.js
+++ js/lobby_menu.js
@@ -1,68 +1,82 @@
-let activeEntryMiddle = null;
+const lobbyPlayersSel = $("#lobby_players");
 
-$(".menu_category").click(clickCategory);
+let playersSlots = 0;
+let playersCount = 0;
 
-function triggerCategory(triggeredCategory) {
-  // Return if empty
-  if (triggeredCategory.is($(".menu_entry_empty_double"))) return;
-  if (triggeredCategory.is($(".menu_entry_empty"))) return;
-  if (activeCategory == null) {
-    triggeredCategory.trigger("categoryActive");
-    console.log("Triggered category: " + triggeredCategory.attr("id"));
-  } else if (activeCategory.is(triggeredCategory)) {
-    console.log("Triggered already active category, will only enter entries middle: " + triggeredCategory.attr("id"));
-    // Enter entries middle
-    if (!triggeredCategory.is(".menu_category_enter")) return;
-    if (!isCategorySelected) {
-      console.log("Entering entries middle from triggerCategory");
-      enterMenuEntriesMiddle(activeWindow.cats[triggeredCategory.index()]);
-    } else if (isCategorySelected) {
-      escapeMenuEntriesMiddle();
-    }
-  } else if (activeCategory != triggeredCategory) {
-    escapeMenuEntriesMiddle();
-    disableCategory(activeCategory);
-    activeCategory = triggeredCategory;
-    triggeredCategory.trigger("categoryActive");
-    console.log("Triggered category: " + triggeredCategory.attr("id"));
+class LobbyPlayer {
+  name = "DefaultName";
+  rank = 100;
+  statusFlag = 0;
+  controlFlag = 1;
+
+  constructor(playerName, playerRank, statusFlag, controlFlag) {
+    this.name = playerName;
+    this.rank = playerRank;
+    this.statusFlag = statusFlag;
+    this.controlFlag = controlFlag;
+  }
+
+  add() {}
+}
+
+function setLobbyInfo(titleString, descrString, creatorString, minRank, playersCount, jobType) {
+  let headerSel = $("#menu_header_text");
+  let descrSel = $("#menu_header_descr");
+  let detailsSel = $("#lobby_details_title_text");
+  let creatorSel = $("#lobby_details_creator_text");
+  let rankSel = $("#lobby_details_rank_text");
+  let playersSel = $("#lobby_details_players_text");
+  let typeSel = $("#lobby_details_type_text");
+
+  headerSel.text(titleString);
+  descrSel.text(descrString);
+
+  detailsSel.text(titleString);
+  creatorSel.text(creatorString);
+  rankSel.text(minRank);
+  playersSel.text(playersCount);
+  typeSel.text(jobType);
+}
+
+function addLobbyPlayer(playerName, playerRank, statusFlag, controlFlag) {
+  if (playersCount >= playersSlots) {
+    console.warn(`Can't add more players as all ${playersSlots} slots are already full`);
+    return;
+  }
+
+  let playerStatuses = [
+    { text: "HOST", class: "player_status_host" },
+    { text: "JOINING", class: "player_status_joining" },
+    { text: "JOINED", class: "player_status_joined" },
+    { text: "INVITED", class: "player_status_invited" },
+    { text: "BLOCKED", class: "player_status_blocked" },
+  ];
+  let playerControls = ["images/icons/control_pad.svg", "images/icons/control_mouse.svg"];
+
+  let blankPlayer = $(`<button id="lobby_players_${playersCount}" class="menu_entry">
+  <span class="player_joined"></span><span class="entry_label element_label_cond">${playerName}</span><div class="element_list">
+  <span class="player_status ${playerStatuses[statusFlag].class}">${playerStatuses[statusFlag].text}</span>
+  <img class="player_control" src="${playerControls[controlFlag]}" alt="">
+  <div class="player_rank"><span class="player_rank_bg"></span><span class="player_rank_icon"></span>
+  <span class="player_rank_number player_rank_number_smaller">${playerRank}</span></div></div></button>`);
+
+  lobbyPlayersSel.children().eq(playersCount).replaceWith(blankPlayer);
+  playersCount++;
+}
+
+function fillPlayerSlots(lobbySlots) {
+  playersSlots = lobbySlots;
+  for (let i = 0; i < lobbySlots; i++) {
+    let blankPlayer = $(`<button id="lobby_players_${i}" class="menu_entry menu_entry_empty"></button>`);
+    lobbyPlayersSel.append(blankPlayer);
   }
 }
 
-function clickCategory() {
-  setEntryActive($(this));
-  if ($(this).attr("id")) console.log("Clicked: " + $(this).attr("id"));
-  else console.log("Clicked menu_entry without ID, possibly menu_entry_empty triggerCategory will return before doing anything");
-}
+window.addEventListener("keydown", function (e) {
+  if (["KeyF"].indexOf(e.code) > -1) {
+    addLobbyPlayer("GTADev" + playersCount, Math.round(Math.random() * 1500), 0, 1);
+  }
+});
 
-function clickEntry() {
-  triggerEntry($(this));
-  if ($(this).attr("id")) console.log("Clicked: " + $(this).attr("id"));
-  else console.log("Clicked menu_entry without ID, possibly menu_entry_empty, triggerEntry will return before doing anything");
-}
-
-function setEntryActive(activatedEntry) {
-  if (activatedEntry.is($(".menu_entry_empty"))) return;
-
-  if (activeEntryMiddle) setEntryDisabled(activeEntryMiddle);
-
-  activeEntryMiddle = activatedEntry;
-  activatedEntry.addClass("menu_entry_active");
-  activatedEntry.focus();
-
-  console.log("Set entry active: " + activatedEntry.attr("id"));
-
-  let rightLabel = activatedEntry.find(".element_list").children(".element_label_right").first();
-  rightLabel.nextAll().hide();
-  if (rightLabel.length != 0) setRightTextArrows(rightLabel);
-}
-
-function setEntryDisabled(disabledEntry) {
-  disabledEntry.removeClass("menu_entry_active");
-  activeEntryMiddle = null;
-  if (activeCategory) activeCategory.focus();
-
-  console.log("Set entry disabled: " + disabledEntry.attr("id"));
-
-  let rightLabel = disabledEntry.find(".element_list").children(".element_label_right").first();
-  if (rightLabel.length != 0) commonMenu.removeRightTextArrows(rightLabel);
-}
+fillPlayerSlots(5);
+setLobbyInfo("Frontend Lobby Menu", "Job brief description.", "GTAMen", 15, 8, "Heist Finale");
